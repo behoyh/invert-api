518160b1b897ae802d62d1c2d3587e48d7e3ffc6
beshoy hanna <beshoyhanna@Beshoys-MacBook.hsd1.mi.comcast.net>
2019-12-28 14:13:36 -0500

    Move https redirect

--- Startup.cs
+++ Startup.cs
@@ -63,16 +63,14 @@ namespace invert_api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
                 app.UseCors("dev");
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
